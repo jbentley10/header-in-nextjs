adabe651a7614378779dd89e5f656d9a4867d453
TSS\jbentley <jbentley@tripwire.com>
2019-09-12 10:42:31 -0700

    Added a CSS file loader, and got the blobs to work, even though they are wonky!

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Logo from './Logo';
-import {useSpring, animated, config} from 'react-spring';
+import {useSpring, useTrail, animated, config} from 'react-spring';
+
+import '../styles.css';
 
 /* Animations */ 
 function HeaderFadeInAnimation() {
@@ -25,6 +27,30 @@ function LogoCircleAnimation() {
   const props = useSpring({ value: 100, from: { value: 0 } })
 
   return <AnimatedDonut percent={props.value} />
+}
+
+/* Blobs animation */
+const fast = { tension: 1200, friction: 40 }
+const slow = { mass: 10, tension: 200, friction: 50 }
+const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
+
+function Goo() {
+  const [trail, set] = useTrail(3, () => ({ xy: [0, 0], config: i => (i === 0 ? fast : slow) }))
+  return (
+    <>
+      <svg style={{ position: 'absolute', width: 0, height: 0 }}>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+          <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
+        </filter>
+      </svg>
+      <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}>
+        {trail.map((props, index) => (
+          <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 /* CSS Styles */ 
@@ -75,6 +101,7 @@ const Header = () => (
       <HeaderFadeInAnimation />
       <SubtitleFadeInAnimation />
     </div>
+    <Goo />
 
     <style jsx>{`
       h1 {
