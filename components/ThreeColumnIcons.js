ba8bca31a306b8aca731732989f3620ba391e46d
John Bentley <jwbentle@gmail.com>
2019-12-13 14:23:50 -0800

    Removed box shadow on 3 column icons.

--- components/ThreeColumnIcons.js
+++ components/ThreeColumnIcons.js
@@ -7,7 +7,7 @@ const ThreeColumnIcons = () => (
     <div className="three-column-icons__content-row inline w-4/5 m-0 text-center">
         <div className="three-column-icons__left inline-block p-12 mb-7">
             <div className="three-column-icons__icon w-20 h-20 pb-8 margin-0-auto">
-                <FontAwesomeIcon icon="check-square" className="text-6xl" />
+                <FontAwesomeIcon icon="check-square" className="text-6xl shadow-none" />
             </div>
             <div className="three-column-icons__text text-left mt-8">
                 <h3 className="text-3xl">Heading!</h3>                
@@ -15,7 +15,7 @@ const ThreeColumnIcons = () => (
         </div>
         <div className="three-column-icons__middle inline-block p-12 mt-6">
             <div className="three-column-icons__icon w-20 h-20 margin-0-auto">
-                <FontAwesomeIcon icon="check-square" className="text-6xl" /> 
+                <FontAwesomeIcon icon="check-square" className="text-6xl shadow-none" /> 
             </div>
             <div className="three-column-icons__text text-left mt-8">
                 <h3 className="text-3xl">Heading!</h3>                
@@ -23,7 +23,7 @@ const ThreeColumnIcons = () => (
         </div>
         <div className="three-column-icons__right inline-block p-12 mb-7">
             <div className="three-column-icons__icon w-20 h-20 pb-8 margin-0-auto">
-                <FontAwesomeIcon icon="check-square" className="text-6xl" />
+                <FontAwesomeIcon icon="check-square" className="text-6xl shadow-none" />
             </div>
             <div className="three-column-icons__text text-left mt-8">
                 <h3 className="text-3xl">Heading!</h3>                
