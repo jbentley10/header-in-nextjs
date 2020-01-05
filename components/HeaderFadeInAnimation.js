d6b54f4b5092358b3c05097a206d78d9d7e78ed7
John Bentley <jwbentle@gmail.com>
2020-01-05 15:02:17 -0800

    Added new files.

---
+++ components/HeaderFadeInAnimation.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types'
+import {useSpring, animated} from 'react-spring';
+
+import '../styles/styles.css';
+
+/* Animations */ 
+const HeaderFadeInAnimation = (props) => {
+  const {
+    TagName:
+      headingText,
+      subtext
+  } = props
+
+  // Define one of our springs
+  const fadeInQuick = useSpring({delay: 4000, opacity: 1, from: {opacity: 0}})
+  const fadeInSlow = useSpring({delay: 5200, opacity: 1, from: {opacity: 0}})
+
+  return (
+    <div className="container">
+      <animated.h1 className="text-6xl" style={fadeInQuick}>{headingText}</animated.h1>
+      <br />
+      <animated.h2 className="text-4xl" style={fadeInSlow}>{subtext}</animated.h2>
+    </div>
+  )
+}
+
+HeaderFadeInAnimation.propTypes = {
+  headingText: PropTypes.string,
+  subtext: PropTypes.string,
+}
+
+HeaderFadeInAnimation.defaultProps = {
+  headingText: '',
+  subtext: ''
+}
+
+export default HeaderFadeInAnimation;
