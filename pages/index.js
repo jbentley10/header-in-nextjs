0833545f56f6a8a0c54e0c93834c8c5582d05067
John Bentley <jwbentle@gmail.com>
2020-01-15 15:12:20 -0800

    Testing removing the <head> code from index.js to see if that fixes the arrow.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import '../styles/styles.css';
-import Head from 'next/head';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <link rel="shortcut icon" href="/static/favicon.ico" />
-        <title>John Bentley - Freelance Web Designer</title>
-        <meta name="description" content="I am a freelance web designer and developer based in Portland, OR. Contact me now to elevate 
-        your brand and drive results."/>
-      </Head>
       <Layout />
     </div>
   );
