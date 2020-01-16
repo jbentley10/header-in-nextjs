35ba87565c9087debe8fc0f4f76ff7e978e03582
John Bentley <jwbentle@gmail.com>
2020-01-15 16:57:39 -0800

    Following these tutorials for react spring: https://alligator.io/react/components-viewport-react-visibility-sensor/, https://css-tricks.com/creating-animations-using-react-spring/

--- components/ThreeColumnIcons.js
+++ components/ThreeColumnIcons.js
@@ -1,39 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link';
+import VizSensor from 'react-visibility-sensor';
 import '../styles/styles.css';
+import { render } from 'react-dom';
 
 /* Component Layout */
-const ThreeColumnIcons = () => (
-  <div className="three-column-icons bg-dark-background sm:block md:flex text-white w-full sm:text-center">
-    <Link href='/work#pueblo-unido-pdx'>
-      <div className="move-up three-column-icons__container bg-dark-background sm:block md:inline-block sm:py-12 md:p-8 mb-7">
-        <div className="bg-cool-green three-column-icons__icon w-full sm:h-auto md:h-full rounded-lg pb-8 margin-0-auto opacity-100 border-8 border-cool-green--reduced-opacity">
-          <img className="opacity-100" src="../static/pueblo-unido/pueblo-unido-mockup.png" />
-          <h3 className="text-2xl sm:opacity-100 md:opacity-0 sm:pb-0">Design and Develop</h3>
-        </div>
+function ThreeColumnIcons () {
+  state = {
+    imgIsVisible = false
+  }
+
+  return (
+    <VizSensor 
+      onChange = {(isVisible) => {
+        this.setState({imgViz: isVisible})
+      }}
+    >
+      <div className="three-column-icons bg-dark-background sm:block md:flex text-white w-full sm:text-center">
+        <Link href='/work#pueblo-unido-pdx'>
+          <div className="move-up three-column-icons__container bg-dark-background sm:block md:inline-block sm:py-12 md:p-8 mb-7">
+            <div className="bg-cool-green three-column-icons__icon w-full sm:h-auto md:h-full rounded-lg pb-8 margin-0-auto opacity-100 border-8 border-cool-green--reduced-opacity">
+              <img className="opacity-100" src="../static/pueblo-unido/pueblo-unido-mockup.png" />
+              <h3 className="text-2xl sm:opacity-100 md:opacity-0 sm:pb-0">Design and Develop</h3>
+            </div>
+          </div>
+        </Link>
+        <Link href='/work#pueblo-unido-pdx'>
+          <div className="three-column-icons__container bg-dark-background sm:block md:inline-block sm:py-12 md:p-8 md:mt-6">
+            <div className="bg-cool-blue three-column-icons__icon w-full sm:h-auto md:h-full rounded-lg margin-0-auto opacity-100 border-8 border-cool-blue--reduced-opacity">
+              <img className="opacity-100" src="../static/pueblo-unido/pueblo-unido-mockup.png" />
+              <h3 className="text-2xl sm:opacity-100 md:opacity-0 sm:pb-8">Build Your Brand</h3>
+            </div>
+          </div>
+        </Link>
+        
+        <Link href='/work#pueblo-unido-pdx'>
+          <div className="move-up three-column-icons__container bg-dark-background sm:block md:inline-block sm:py-12 md:p-8 mb-7">
+            <div className="bg-cool-purple three-column-icons__icon w-full sm:h-auto md:h-full rounded-lg pb-8 margin-0-auto opacity-100 border-8 border-cool-purple--reduced-opacity">
+              <img className="opacity-100" src="../static/tripwire/tripwire-mockup.png" />
+              <h3 className="text-2xl sm:opacity-100 md:opacity-0 sm:pb-0">Drive Results</h3>
+            </div>
+          </div>
+        </Link>
       </div>
-    </Link>
-    <Link href='/work#pueblo-unido-pdx'>
-      <div className="three-column-icons__container bg-dark-background sm:block md:inline-block sm:py-12 md:p-8 md:mt-6">
-        <div className="bg-cool-blue three-column-icons__icon w-full sm:h-auto md:h-full rounded-lg margin-0-auto opacity-100 border-8 border-cool-blue--reduced-opacity">
-          <img className="opacity-100" src="../static/pueblo-unido/pueblo-unido-mockup.png" />
-          <h3 className="text-2xl sm:opacity-100 md:opacity-0 sm:pb-8">Build Your Brand</h3>
-        </div>
-      </div>
-    </Link>
-    
-    <Link href='/work#pueblo-unido-pdx'>
-      <div className="move-up three-column-icons__container bg-dark-background sm:block md:inline-block sm:py-12 md:p-8 mb-7">
-        <div className="bg-cool-purple three-column-icons__icon w-full sm:h-auto md:h-full rounded-lg pb-8 margin-0-auto opacity-100 border-8 border-cool-purple--reduced-opacity">
-          <img className="opacity-100" src="../static/tripwire/tripwire-mockup.png" />
-          <h3 className="text-2xl sm:opacity-100 md:opacity-0 sm:pb-0">Drive Results</h3>
-        </div>
-      </div>
-    </Link>
-    <style jsx>{`
-      
-    `}</style>
-  </div>
-);
+    </VizSensor>
+  )
+}
 
 export default ThreeColumnIcons;
