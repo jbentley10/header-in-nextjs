d50f988300fad901f91cfc1bd49c5c85af164dc4
John Bentley <jwbentle@gmail.com>
2020-01-20 14:54:28 -0800

    Added sitemap for users.

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,5 +1,4 @@
 import '../styles/styles.css';
-import Link from 'next/link';
 
 import Button from '../components/Button';
 import Layout from '../components/Layout';
---
+++ pages/sitemap.js
@@ -0,0 +1,47 @@
+import '../styles/styles.css';
+
+import Button from '../components/Button';
+import Layout from '../components/Layout';
+import ImageHeader from '../components/ImageHeader';
+import DotsTransition from '../components/Images/DotsTransition';
+
+export default function Home() {
+    return (
+      <div>
+        <Layout 
+          pageMeta={{
+            title: " | sitemap",
+            keywords: ["freelance", "web designer", "web developer", "portland", "oregon"],
+            description: "View the sitemap for jwbentley.com",
+            URL: "https://www.jwbentley.com/about-me"
+          }}
+          navigationClassName={'lg:h-128 navigation h-56'}
+          fadeInNav={false}
+          logoWidth={'7%'}
+          logoClassName={'relative cursor-pointer logo'}
+          drawAnimations={false}
+        >
+        <ImageHeader 
+            heading={"Sitemap"}
+        />
+
+        <div className="background-dots text-container w-full sm:p-8 md:p-24 lg:p-0">
+          <div className="text-content sm:p-4 md:px-24">
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p className="text-white lg:w-4/6 m-auto"><a href="/work" className="text-link-purple hover:text-link-purple-darker cursor-pointer">Work</a></p>
+            <p>&nbsp;</p>
+            <p className="text-white lg:w-4/6 m-auto"><a href="/about-me" className="text-link-purple hover:text-link-purple-darker cursor-pointer">About Me</a></p>
+            <p>&nbsp;</p>
+            <p className="text-white lg:w-4/6 m-auto"><a href="/services" className="text-link-purple hover:text-link-purple-darker cursor-pointer">Services</a></p>
+            <p>&nbsp;</p>
+            <p className="text-white lg:w-4/6 m-auto"><a href="/contact" className="text-link-purple hover:text-link-purple-darker cursor-pointer">Contact</a></p>
+          </div>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div> 
+      </Layout>
+      
+    </div>
+  );
+}
