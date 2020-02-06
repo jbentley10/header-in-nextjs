63299f7ac571e9de53844f47723795f602110336
John Bentley <jwbentle@gmail.com>
2020-02-06 11:05:59 -0800

    Updated email to professional email.

--- pages/contact.js
+++ pages/contact.js
@@ -11,7 +11,7 @@ const MainText = () =>
     <h2 className="text-3xl">What are you waiting for? It's your time to shine.</h2>
     <p>&nbsp;</p>
     <p className="text-lg">Contact me directly via phone or email: </p> 
-    <p><FontAwesomeIcon icon="envelope" className="text-6xl shadow-none p-4 cursor-pointer inline-block align-middle" /><a href="mailto:jwbentle@gmail.com" className="text-link-purple hover:text-link-purple-darker align-middle"> jwbentle@gmail.com</a></p>
+    <p><FontAwesomeIcon icon="envelope" className="text-6xl shadow-none p-4 cursor-pointer inline-block align-middle" /><a href="mailto:john@jwbentley.com" className="text-link-purple hover:text-link-purple-darker align-middle"> john@jwbentley.com</a></p>
     <p><FontAwesomeIcon icon="phone" className="text-6xl shadow-none p-4 cursor-pointer inline-block align-middle" /><a href="tel:+17605324516" className="text-link-purple hover:text-link-purple-darker align-middle"> (760) 532-4516</a></p>
     <p className="text-lg">I am currently accepting clients.</p> 
     <p>&nbsp;</p>
