52a11d8e77d9261416251829ba3f13df05447b82
John Bentley <jwbentle@gmail.com>
2020-02-13 20:53:43 -0800

    Working on getting the images to show up properly in Safari.

--- components/Images/DotsTransition.js
+++ components/Images/DotsTransition.js
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 
+function addDefaultSrc(ev) {
+  ev.target.src = '../static/dots-transition.jpg'
+}
+
 const DotsTransition = (props) => {
   const {
     tagName: tag,
     className
   } = props
 
-  return <img className={className + " bg-dark-background w-full"} src="../static/dots-transition.webp" onerror="this.src='../static/dots-transition.jpg'" alt="Circles and Dots" />
+  return <img className={className + " bg-dark-background w-full"} src="../static/dots-transition.webp" onError={addDefaultSrc} alt="Circles and Dots" />
 }
 
 DotsTransition.propTypes = {
