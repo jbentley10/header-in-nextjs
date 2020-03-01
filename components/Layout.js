bacf3e46ef4a9576bb2628ca8862e1f44b5644d5
John Bentley <jwbentle@gmail.com>
2020-03-01 15:26:48 -0800

    Trying a different way to use GTM.

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import '../styles/styles.css';
 import Head from 'next/head'
-import React from 'react'
+import React, { Component } from 'react'
 import TagManager from 'react-gtm-module'
 
 import Navigation from './Navigation';
@@ -17,51 +17,55 @@ const tagManagerArgs = {
   id: 'GTM-MX6MMQH'
 }
 
-const Layout = ({ pageMeta, children, navigationClassName, fadeInNav, logoWidth, logoClassName, drawAnimations, animationClassName }) => (
-  <>
-    <Head>
-      <title>{`${pageMeta.title}`}</title>
+class Layout extends Component {
+  componentDidMount = async function() {
+    TagManager.initialize(tagManagerArgs)
+  }
 
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="author" content="John Bentley" />
+  render() {
+    return (
+      <>
+        <Head>
+          <title>{`${this.props.pageMeta.title}`}</title>
 
-      <meta name="description" content={pageMeta.description} />
-      <meta name="google-site-verification" content="M_p92ZTam2mOfgHf3dENr7BjOADVdOWvwp4RjHG8reY" />
-      <meta name="google-site-verification" content="_w00_Eeow8a5B9kCntT5NbQKkc71Jc-16sukIt5qAIc" />
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="author" content="John Bentley" />
 
-      <meta name="keywords" content={pageMeta.keywords.join(',')} />
+          <meta name="description" content={this.props.pageMeta.description} />
+          <meta name="google-site-verification" content="M_p92ZTam2mOfgHf3dENr7BjOADVdOWvwp4RjHG8reY" />
+          <meta name="google-site-verification" content="_w00_Eeow8a5B9kCntT5NbQKkc71Jc-16sukIt5qAIc" />
 
-      <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="keywords" content={this.props.pageMeta.keywords.join(',')} />
 
-      <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet" async />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
 
-      <link rel="canonical" href={pageMeta.URL} />
-    </Head>
+          <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet" async />
 
-    {/* Google Tag Manager */}
-    <noscript>{`
-    {
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX6MMQH"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    }
-    `}</noscript>
-    
-    <Navigation 
-      navigationClassName={navigationClassName}
-      fadeInNav={fadeInNav}
-      logoWidth={logoWidth}
-      logoClassName={logoClassName}
-      drawAnimations={drawAnimations}
-      animationClassName={animationClassName}
-    />
-      {children}
-    <Footer />
-  </>
-);
+          <link rel="canonical" href={this.props.pageMeta.URL} />
+        </Head>
 
-Layout.componentDidMount = async function() {
-  TagManager.initialize(tagManagerArgs)
-}
+        {/* Google Tag Manager */}
+        <noscript>{`
+        {
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX6MMQH"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        }
+        `}</noscript>
+        
+        <Navigation 
+          navigationClassName={this.props.navigationClassName}
+          fadeInNav={this.props.fadeInNav}
+          logoWidth={this.props.logoWidth}
+          logoClassName={this.props.logoClassName}
+          drawAnimations={this.props.drawAnimations}
+          animationClassName={this.props.animationClassName}
+        />
+          {this.props.children}
+        <Footer />
+      </>
+    )
+  }
+};
 
 export default Layout;
