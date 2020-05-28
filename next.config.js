1d9450e4eb77ea6df7675032b729d13015c4b92a
John Bentley <jwbentle@gmail.com>
2020-05-27 17:07:46 -0700

    Removed purgeCSS stuff (for now).

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 // next.config.js
-const withCSS = require('@zeit/next-css');
-const withPurgeCss = require('next-purgecss');
-module.exports = withCSS(
-  withPurgeCss({
-    purgeCssEnabled: ({ dev, isServer }) => (!dev && !isServer) // Only enable PurgeCSS for client-side production builds
-  })
-)
-
-// next.config.js
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS({
+  /* config options here */
+})
